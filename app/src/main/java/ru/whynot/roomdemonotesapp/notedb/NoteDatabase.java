6db45513ab9e4cbe7723b317b23fd6a3e8e959e4
CleanTopic@yandex.ru <Danila.koksharov2002@gmail.com>
2022-06-21 20:59:58 +0300

    That is a room project

---
+++ app/src/main/java/ru/whynot/roomdemonotesapp/notedb/NoteDatabase.java
@@ -0,0 +1,41 @@
+package com.whynot.roomdemonotesapp.notedb;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.whynot.roomdemonotesapp.notedb.dao.NoteDao;
+import com.whynot.roomdemonotesapp.notedb.model.Note;
+import com.whynot.roomdemonotesapp.util.Constants;
+import com.whynot.roomdemonotesapp.util.DateRoomConverter;
+
+@Database(entities = { Note.class }, version = 1)
+@TypeConverters({DateRoomConverter.class})
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract NoteDao getNoteDao();
+
+
+    private static NoteDatabase noteDB;
+
+    // synchronized is use to avoid concurrent access in multithred environment
+    public static /*synchronized*/ NoteDatabase getInstance(Context context) {
+        if (null == noteDB) {
+            noteDB = buildDatabaseInstance(context);
+        }
+        return noteDB;
+    }
+
+    private static NoteDatabase buildDatabaseInstance(Context context) {
+        return Room.databaseBuilder(context,
+                NoteDatabase.class,
+                Constants.DB_NAME).allowMainThreadQueries().build();
+    }
+
+    public  void cleanUp(){
+        noteDB = null;
+    }
+}
